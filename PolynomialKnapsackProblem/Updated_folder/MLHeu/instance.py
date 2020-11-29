fa9d6cab8fbd5f841ee3aa75bc8edf7f4f8ff03b
Lorenzo Cascioli <44396386+loricrash6@users.noreply.github.com>
2020-11-29 13:11:36 +0100

    Add files via upload

---
+++ PolynomialKnapsackProblem/Updated_folder/MLHeu/instance.py
@@ -0,0 +1,46 @@
+import logging
+import random
+import numpy as np
+import json
+import math
+
+
+class Instance():
+    def __init__(self, sim_setting):
+        """[summary]
+        
+        Arguments:
+            sim_setting {[type]} -- [description]
+        """
+        logging.info("starting simulation...")
+        #self.max_size = sim_setting['knapsack_size']
+        self.max_size = sim_setting['budget']
+        self.sizes = np.around(np.random.uniform(
+            sim_setting['n_items']
+        ))
+        self.profits = sim_setting['profits']
+        self.costs = sim_setting['costs']
+        self.polynomial_gains = sim_setting['polynomial_gains']
+        self.n_items = sim_setting['n_items']
+        #self.gamma = sim_setting['Gamma']
+        self.gamma = sim_setting['gamma']
+        logging.info("simulation end")
+
+
+    def get_data(self):
+        """[summary]
+        
+        Returns:
+            [type] -- [description]
+        """
+        logging.info("getting data from instance...")
+        return {
+            "profits": self.profits,
+            "sizes": self.sizes,
+            "budget": self.max_size,
+            "n_items": self.n_items,
+            "costs": self.costs,
+            "polynomial_gains": self.polynomial_gains,
+            "gamma": self.gamma
+        }
+
